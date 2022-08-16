065398f1ae9e1db1de756b7fbed0f239003c58b6
jreydc <jreydc@gmail.com>
2022-08-16 17:01:13 +0800

    Player Animator Implementation

---
+++ Galaga2DProject/Assets/Scripts/UnitAnimator.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class UnitAnimator : MonoBehaviour
+{
+    const string PLAYER_IDLE = "Idle";
+    const string PLAYER_ATTACK = "Attack";
+    const string PLAYER_MOVING = "Moving";
+    
+    private float xAxis;
+    private float yAxis;
+    private float attackDelay = 0.3f;
+
+    private Animator anime2r;
+    private string currentAnime2RState;
+    private bool isAttacking;
+
+    private void Awake() {
+        anime2r = GetComponent<Animator>();
+        //isAttacking = false;
+    }
+
+    public void ChangeAnimationState(string newState){
+        //stop the same animation from interrupting itself
+        if (currentAnime2RState == newState) return;
+
+        //play the animation
+        anime2r.Play(stateName: newState);
+
+        currentAnime2RState = newState;
+    }
+
+    public void PlayerAttack(){
+        attackDelay = anime2r.GetCurrentAnimatorStateInfo(0).length;
+        Invoke("AttackComplete2Idle", attackDelay);
+    }
+
+    public void PlayerMoving(){
+
+        ChangeAnimationState(PLAYER_MOVING);
+        attackDelay = anime2r.GetCurrentAnimatorStateInfo(0).length;
+        Invoke("AttackComplete2Idle", attackDelay);
+    }
+
+    
+    public void PlayerIdle(){
+        ChangeAnimationState(PLAYER_IDLE);
+    }
+    /* void AttackComplete2Idle()
+    {
+        //isAttacking = false;
+        ChangeAnimationState(PLAYER_IDLE);
+    } */
+}
