7b7a852163e244d03dde2cc15f28be1773d167df
jreydc <jreydc@gmail.com>
2022-08-17 11:44:51 +0800

    Update Player.cs

--- Galaga2DProject/Assets/Scripts/Player.cs
+++ Galaga2DProject/Assets/Scripts/Player.cs
@@ -14,6 +14,7 @@ public class Player : Unit
     void Start()
     {
         unitMovementController = GetComponent<UnitMovementController>();
+        unitAnim82r = 
     }
 
     // Update is called once per frame
