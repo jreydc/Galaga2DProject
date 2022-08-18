473e7ac12ac128d5625c2a0be83a3bad3804c0bb
jreydc <jreydc@gmail.com>
2022-08-18 09:44:01 +0800

    Enemy Booster Adjustment

--- Galaga2DProject/Assets/Scripts/Enemy.cs
+++ Galaga2DProject/Assets/Scripts/Enemy.cs
@@ -5,23 +5,20 @@ using UnityEngine;
 public class Enemy : Unit
 {
     private UnitAnimator unitAnim82r;
-    [SerializeField]private float time2Move = 0f;
+    [SerializeField]private float time2Move;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        unitAnim82r = transform.GetChild(0).gameObject.GetComponent<UnitAnimator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         time2Move += Time.deltaTime;
-        if(time2Move >= 5f){
+        if(time2Move >= 10f){
             unitAnim82r.PlayerMoving();
-        }else{
-            unitAnim82r.PlayerIdle();
-            time2Move = 0f;
         }
         
     }
