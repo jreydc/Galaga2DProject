88617753254d58520d63f285a548c5f33a1833c0
jreydc <jreydc@gmail.com>
2022-08-24 18:37:52 +0800

    Update ResourceSystem.cs

--- Galaga2DProject/Assets/_Scripts/Systems/ResourceSystem.cs
+++ Galaga2DProject/Assets/_Scripts/Systems/ResourceSystem.cs
@@ -2,17 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// One repository for all scriptable objects. Create your query methods here to keep your business logic clean.
+/// </summary>
+
 public class ResourceSystem : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public List<UnitBase> units { get; private set; }
+    private Dictionary<Faction, UnitBase> _unitDict;
+
+    protected void Awake() {
+        AssembleResources();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void AssembleResources() {
+        units = Resources.LoadAll<UnitBase>("Prefabs").ToList();
+        _unitDict = units.ToDictionary(r => r.Faction, r => r);
     }
+
+    public UnitBase GetUnit(Faction t) => _unitDict[t];
+    public UnitBase GetRandomHero() => units[Random.Range(0, units.Count)];
 }
