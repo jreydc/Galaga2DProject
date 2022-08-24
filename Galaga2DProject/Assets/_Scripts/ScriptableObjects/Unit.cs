96f4cddda5a33b47ea1d41501a87f1c525065f8c
jreydc <jreydc@gmail.com>
2022-08-24 17:39:21 +0800

    Update Unit.cs

--- Galaga2DProject/Assets/_Scripts/ScriptableObjects/Unit.cs
+++ Galaga2DProject/Assets/_Scripts/ScriptableObjects/Unit.cs
@@ -4,9 +4,7 @@
 public class Unit : ScriptableObject { 
     public Faction _faction;
     [SerializeField]private UnitStats _stats;
-    
     public UnitStats BaseStats => _stats;
-
     public UnitBase unitPrefab;
     public Projectile _projectile;
     
