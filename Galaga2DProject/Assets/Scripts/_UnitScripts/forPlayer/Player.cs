600efd7645d9c75fe84f080f4ea32c202addd573
jreydc <jreydc@gmail.com>
2022-08-24 12:50:10 +0800

    Formulating Libraries on Timer

--- Galaga2DProject/Assets/Scripts/_UnitScripts/forPlayer/Player.cs
+++ Galaga2DProject/Assets/Scripts/_UnitScripts/forPlayer/Player.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(CapsuleCollider2D))]
 public class Player : MonoBehaviour
